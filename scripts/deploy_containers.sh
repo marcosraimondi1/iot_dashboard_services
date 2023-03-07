dc8b08b82a4bb24864048110f0a5d59bc6070824
Marcos Raimondi <marcosraimondi1@hotmail.com>
2023-03-07 13:54:10 -0300

    deploy containers script

--- scripts/deploy_containers.sh
+++ scripts/deploy_containers.sh
@@ -1,10 +1,13 @@
 # deploy containers
 if [ $FIRST_DEPLOY ]; then
     echo "installing node_modules and building app"
+    sudo docker-compose -f docker-compose-production.yml down
+    sudo docker-compose -f docker_node_install.yml down
     sudo docker-compose -f docker_node_install.yml up && echo "starting server" && sudo docker-compose -f docker-compose-production.yml up -d
     sudo sh -c "echo FIRST_DEPLOY=false >> /etc/environment"
     source /etc/environment
 else
     echo "starting server"
+    sudo docker-compose -f docker-compose-production.yml build
     sudo docker-compose -f docker-compose-production.yml up -d
 fi

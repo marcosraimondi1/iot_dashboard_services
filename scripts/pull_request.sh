3594ec3054ca61881a0fdc719d568d2f316b629b
Marcos Raimondi <marcosraimondi1@hotmail.com>
2023-03-07 12:36:19 -0300

    pull req

--- scripts/pull_request.sh
+++ scripts/pull_request.sh
@@ -5,21 +5,21 @@ SERVICES=https://github.com/marcosraimondi1/iot_dashboard_services.git
 APP=https://github.com/marcosraimondi1/iot_dashboard.git
 
 # check if directory exists
-if [ -d "~/$DIR" ];
+if [ -d "./$DIR" ];
 then
     # pull repo 
     echo "pulling repo"
-    cd "~/$DIR"
+    cd "./$DIR"
     sudo git pull
     cd ./app
     sudo git pull
 else
     # clone repo
-    cd ~
     echo "cloning repo"
     sudo git clone "$SERVICES" "$DIR"
-    cd "$DIR"
+    cd "./$DIR"
     sudo git clone "$APP" app
 fi
 
-export FIRST_DEPLOY=true
+sudo sh -c "echo FIRST_DEPLOY=true >> /etc/environment"
+source /etc/environment
